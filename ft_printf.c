fbb33795eb9eccea59b69452b7e1a2a6fc49ecc6
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-05-27 13:44:59 +0300

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -1,66 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dakcakoc <dakcakoce@student.hive.fi>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/27 11:30:16 by dakcakoc          #+#    #+#             */
+/*   Updated: 2024/05/27 11:40:35 by dakcakoc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "ft_printf.h"
 
-# include "ft_printf.h"
-
-int ft_printf(const char *s, ...)
+int	ft_printf(const char *s, ...)
 {
-    if (!s)
-        return -1;
-    va_list args; //va_list is a type to hold information about variable arguments.
-    va_start(args, s); //Simply put, it initializes the va_list variable to point to the first variable argument which is the last known fixed parameter (s in this case).
-    int count;
-    int i;
-    
-    count = 0; //This variable will be used to count the number of characters printed.
-    i = 0;
-    while (s[i])
-    {
-    if (s[i] == '%')
-    {
-        i++; // skip the % character
-        int res = formats(&args, s[i]);
-        if (res < 0)
-            return -1; 
-        count = count + res;
-    }  
-    else {
-        int res = ft_printchar(s[i]);
-        if (res < 0)
-            return -1; 
-        count = count + res;
-    }
-    i++;
-}
-    va_end(args);
-    if (count < 0)
-        return -1;
-    return count;
+	va_list	args;
+	int		count;
+	int		i;
+
+	va_start (args, s);
+	count = 0;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '%')
+		{
+			count += formats(&args, s[++i]);
+			if (count < 0)
+				return (-1);
+		}
+		else
+		{
+			count += ft_printchar(s[i]);
+			if (count < 0)
+				return (-1);
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
 }
 
-int formats(va_list *args, const char format)
+int	formats(va_list *args, const char format)
 {
-    int count;
+	int	count;
 
-    count = 0;
-    if (format == 'd' || format == 'i')
-        count += ft_printnbr(va_arg(*args, int)); 
-    else if (format == 'c')
-        count += ft_printchar((char)va_arg(*args, int));
-    else if (format == 's')
-        count += ft_printstr(va_arg(*args, char *));
-    else if (format == 'x')
-        count += ft_printhex(va_arg(*args, unsigned int), 0);
-    else if (format == 'X')
-        count += ft_printhex(va_arg(*args, unsigned int), 1);
-    else if (format == 'u')
-        count += ft_printunsigned(va_arg(*args, unsigned int));
-    else if (format == 'p')
-        count += ft_printptr(va_arg(*args, void *));
-    else if (format == '%')
-        count += ft_printpercent();
-    else
-        return -1;
-    if (count < 0)
-        return -1; // Propagate the error
-    return count;
+	count = 0;
+	if (format == 'd' || format == 'i')
+		count += ft_printnbr(va_arg(*args, int));
+	else if (format == 'c')
+		count += ft_printchar((char)va_arg(*args, int));
+	else if (format == 's')
+		count += ft_printstr(va_arg(*args, char *));
+	else if (format == 'x')
+		count += ft_printhex(va_arg(*args, unsigned int), 0);
+	else if (format == 'X')
+		count += ft_printhex(va_arg(*args, unsigned int), 1);
+	else if (format == 'u')
+		count += ft_printunsigned(va_arg(*args, unsigned int));
+	else if (format == 'p')
+		count += ft_printptr(va_arg(*args, void *));
+	else if (format == '%')
+		count += ft_printpercent();
+	else
+		return (-1);
+	if (count < 0)
+		return (-1);
+	return (count);
 }
-    
+
+int	ft_printchar(char c)
+{
+	int	result;
+
+	result = write(1, &c, 1);
+	if (result < 0)
+		return (-1);
+	return (1);
+}
+
+int	ft_printstr(char *str)
+{
+	int	count;
+	int	result;
+
+	count = 0;
+	if (!str)
+		str = "(null)";
+	while (*str)
+	{
+		result = ft_printchar(*str);
+		if (result < 0)
+			return (-1);
+		count += result;
+		str++;
+	}
+	if (count < 0)
+		return (-1);
+	return (count);
+}
--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -1,111 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dakcakoc <dakcakoce@student.hive.fi>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/27 11:50:13 by dakcakoc          #+#    #+#             */
+/*   Updated: 2024/05/27 12:42:19 by dakcakoc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "ft_printf.h"
 
-
-int ft_printchar(char c) //This function is used to print a character and return the number of characters printed.
-{
-		int result;
-		result = write(1, &c, 1);
-		if(result < 0)
-			return (-1);
-		return (1);
-}
-
-int ft_printstr(char *str) //This function is used to print a string and return the number of characters printed.
-{
-    int count = 0;
-	int result;
-
-    if (!str)
-        str = "(null)";
-    while (*str)
-    {
-        result = ft_printchar(*str);
-		if (result < 0)
-			return (-1);
-		count += result;
-        str++;
-    }
-	if (count < 0)
-		return (-1);
-    return count;
-}
-
-int	ft_printnbr(int nb) 
+int	ft_printnbr(int nb)
 {
 	int	count;
 
 	count = 0;
 	if (nb == -2147483648)
 	{
-		write(1, "-2147483648", 11);
-		count = 11;
+		count += ft_printstr("-2147483648");
+		if (count < 0)
+			return (-1);
 	}
 	else if (nb < 0)
 	{
 		count += ft_printchar('-');
+		if (count < 0)
+			return (-1);
 		nb = -nb;
 		count += ft_printnbr(nb);
 	}
 	else if (nb > 9)
 	{
 		count += ft_printnbr(nb / 10);
+		if (count < 0)
+			return (-1);
 		count += ft_printnbr(nb % 10);
 	}
 	else
 	{
 		count += ft_printchar(nb + '0');
+		if (count < 0)
+			return (-1);
+	}
+	return (count);
+}
+
+int	ft_printhex(unsigned long long n, int uppercase)
+{
+	int				count;
+	unsigned int	digit;
+
+	count = 0;
+	if (n >= 16)
+	{
+		count += ft_printhex(n / 16, uppercase);
+		if (count < 0)
+			return (-1);
+	}
+	digit = n % 16;
+	if (digit < 10)
+		count += ft_printchar(digit + '0');
+	else
+	{
+		if (uppercase)
+			count += ft_printchar(digit - 10 + 'A');
+		else
+			count += ft_printchar(digit - 10 + 'a');
 	}
 	if (count < 0)
 		return (-1);
 	return (count);
 }
 
-int ft_printhex(unsigned long long n, int uppercase) {
-    int count = 0;
-    if (n >= 16) {
-        count += ft_printhex(n / 16, uppercase);
-    }
-    unsigned int digit = n % 16;
-    if (digit < 10) {
-        ft_printchar(digit + '0');
-    } else {
-        if (uppercase) {
-            ft_printchar(digit - 10 + 'A');
-        } else {
-            ft_printchar(digit - 10 + 'a');
-        }
-    }
-    count++;
-    return count;
-}
-
-int ft_printptr(void *p) {
-    int count = 0;
-
-    count += ft_printstr("0x");
-    count += ft_printhex((unsigned long long)p, 0);  // Pass the pointer cast to unsigned long long
-    return count;
-}
-
-int ft_printunsigned(unsigned int n) //This function is used to print an unsigned integer and return the number of characters printed.
+int	ft_printptr(void *p)
 {
-	int count = 0;
+	int	count;
+
+	count = 0;
+	count += ft_printstr("0x");
+	if (count < 0)
+		return (-1);
+	count += ft_printhex((unsigned long long)p, 0);
+	if (count < 0)
+		return (-1);
+	return (count);
+}
+
+int	ft_printunsigned(unsigned int n)
+{
+	int	count;
+
+	count = 0;
+	if (n < 0)
+		return (-1);
 	if (n >= 10)
 	{
 		count += ft_printunsigned(n / 10);
+		if (count < 0)
+			return (-1);
 	}
-	ft_printchar(n % 10 + '0');
-	count++;
+	count += ft_printchar(n % 10 + '0');
 	if (count < 0)
 		return (-1);
 	return (count);
 }
 
-int ft_printpercent(void) //This function is used to print a percent sign and return the number of characters printed.
+int	ft_printpercent(void)
 {
-    int count = 0;
+	int	count;
+
+	count = 0;
 	count = ft_printchar('%');
 	if (count < 0)
 		return (-1);
-    return (count);
+	return (count);
 }
