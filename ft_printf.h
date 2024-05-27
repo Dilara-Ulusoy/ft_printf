942414f69d05cc032beec46496d02bb9468d0fca
Dilara-Ulusoy <147133001+Dilara-Ulusoy@users.noreply.github.com>
2024-05-27 13:48:42 +0300

    Add files via upload

--- ft_printf.h
+++ ft_printf.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dakcakoc <dakcakoce@student.hive.fi>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/27 13:46:00 by dakcakoc          #+#    #+#             */
+/*   Updated: 2024/05/27 13:48:28 by dakcakoc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
@@ -6,14 +18,14 @@
 # include <stdlib.h>
 # include <limits.h>
 
-int ft_printnbr(int nb);          // Should return the number of characters printed
-int ft_printchar(char c);        // Should return the number of characters printed
-int ft_printstr(char *str);  // Should return the number of characters printed
-int ft_printhex(unsigned long long n, int uppercase); // Should return the number of characters printed
-int ft_printptr(void *p);        // Should return the number of characters printed
-int ft_printpercent(void);       // Should return the number of characters printed          // Should return the number of characters printed (typically 1)
-int ft_printunsigned(unsigned int n);
-int formats(va_list *args, const char format);
-int ft_printf(const char *s, ...);
+int	ft_printnbr(int nb);
+int	ft_printchar(char c);
+int	ft_printstr(char *str);
+int	ft_printhex(unsigned long long n, int uppercase);
+int	ft_printptr(void *p);
+int	ft_printpercent(void);
+int	ft_printunsigned(unsigned int n);
+int	formats(va_list *args, const char format);
+int	ft_printf(const char *s, ...);
 
-# endif
+#endif
